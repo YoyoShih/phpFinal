746a9bcfbad7ef4c9dee54500ce5a1cd131e99e8
YoyoShih <64921618+YoyoShih@users.noreply.github.com>
2021-05-22 00:56:42 +0800

    5/22 ver1

---
+++ views/scripts/search.js
@@ -0,0 +1,47 @@
+setTimeout(() => {
+    var mask = document.getElementsByClassName('mask')[0]
+    var logo = document.getElementsByClassName('logo')[0]
+    var left = document.getElementsByClassName('left')[0]
+    var right = document.getElementsByClassName('right')[0]
+    mask.remove()
+    logo.remove()
+    left.remove()
+    right.remove()
+}, 2000)
+
+searchPeople()
+
+function searchPeople() {
+    fetch('http://localhost/final/phpFinal/models/search.php', {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }).then(response => {
+        response.json().then(result => {
+            //拿到隨機三個人
+        })
+    })
+}
+
+const like = document.getElementsByClassName('like')[0]
+
+function likeOrDislike(obj) {
+    fetch('http://localhost/final/phpFinal/models/likeOrDislike.php', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            like: (obj === like),
+            name: ''
+        })
+    }).then(response => {
+        response.json().then(result => {
+            //多拿到一個人
+        })
+    })
+    //下一個人
+}
